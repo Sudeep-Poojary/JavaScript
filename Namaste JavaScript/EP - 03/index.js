9c01fe0ed206ccab08e6a161111fd9d9de41acfe
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2024-01-21 17:17:20 +0530

    Initial Commit

---
+++ Namaste JavaScript/EP - 03/index.js
@@ -0,0 +1,9 @@
+console.log(x);
+var x = 10;
+
+getName();
+function getName() {
+  console.log("Namaste JavaScript");
+}
+
+console.log(getName);
---
+++ Namaste JavaScript/EP - 06/index.js
@@ -0,0 +1,22 @@
+var a;
+
+// a = 10;
+
+console.log(a);
+
+if (a === undefined) {
+  console.log("a is undefined");
+} else {
+  console.log("a is not undefined");
+}
+
+// JS is a loosely / weakly typed language
+
+var b;
+console.log(b);
+
+b = 100;
+console.log(b);
+
+b = "Hello World";
+console.log(b);
